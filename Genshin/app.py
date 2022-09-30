75c586981ee8dda41bba84b312c43ca8d27d2d91
Wooju0105 <89179011+Wooju0105@users.noreply.github.com>
2022-09-30 13:11:20 +1300

    commit 6

--- Genshin/app.py
+++ Genshin/app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, redirect, session
 from config import Config
 import sqlite3
-import random
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Config) #brings "config" class from the config.py file. 
 
 @app.context_processor
 def header_title():
-  return dict(title=app.config['TITLE'])
+  return dict(title=app.config['TITLE']) #Displays the title on the config.py file as a header. 
 
 
 @app.route("/")
@@ -22,58 +21,49 @@ def characters():
     cur.execute("SELECT * FROM Characters ORDER BY id;") #There are multiple characters’ detailed information on the database and this sql query select all the info from the table on the database and order and display them by the id on the table.
     characters = cur.fetchall() #Fetches all the onfo on the table; CHaracters
     conn.close()
-    return render_template("characters.html", characters=characters) #direct the user to character.html webpage
+    return render_template("characters.html", characters=characters) #Directs the user to character.html webpage
 
-@app.route('/characters/<int:id>')
+@app.route('/characters/<int:id>') #Gets directed from the characters webpage and displays different information of the characters depends on the 'int'.
 def des(id):
-  conn = sqlite3.connect(app.config['DATABASE'])
+  conn = sqlite3.connect(app.config['DATABASE']) #connects the database defined on the config.py.
   cur = conn.cursor()
-  cur.execute("SELECT * FROM Characters WHERE id=?;",(id,))
+  cur.execute("SELECT * FROM Characters WHERE id=?;",(id,)) #Select a certain character from the list of four and directs to its own desciption webpage.
   characters = cur.fetchone()
   conn.close()
-  return render_template("description.html", characters = characters)
+  return render_template("description.html", characters = characters) #Directs the user to description.html.
 
-@app.route('/skills')
+@app.route('/skills') #When the route is to /skills
 def skills():
     conn = sqlite3.connect(app.config['DATABASE'])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM skills ORDER BY id;")
+    cur.execute("SELECT * FROM skills ORDER BY id;") # fetchone returns a certain data for one entry
     skills = cur.fetchall()
     conn.close()
     return render_template("skills.html", skills=skills)
 
-@app.route('/skills/<int:id>')
+@app.route('/skills/<int:id>') #Directs from skills webpage from tuple. 
 def skill_des(id):
   conn = sqlite3.connect(app.config['DATABASE'])
   cur = conn.cursor()
-  cur.execute("SELECT * FROM skills WHERE id=?;",(id,))
+  cur.execute("SELECT * FROM skills WHERE id=?;",(id,)) # fetchone returns a certain data for one entry
   skills = cur.fetchone()
   conn.close()
   return render_template("skills_des.html", skills = skills)
 
-@app.route('/comment', methods=["GET","POST"])
-def comment():
-    if request.method == "POST":
-        conn = sqlite3.connect(app.config['DATABASE'])
-        cur = conn.cursor()
-        title = request.form["title"]
-        feedback = request.form["feedback"]
-        sql = "INSERT INTO "
-
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST']) #GET, POST methods to edit/add on the table of the database for the users details
 def register():
         if request.method == 'GET':
             return render_template("register.html")
 
-        if request.method == 'POST':
+        if request.method == 'POST': #request userid, username, password, re_passowrd; these are the information that would go under each column of the table.
             userid = request.form['userid']
             username = request.form['username']
             password = request.form['password']
             re_password = request.form['re_password']
             conn = sqlite3.connect(app.config['DATABASE'])
             cs = conn.cursor()
-            cs.execute('INSERT INTO user (userid, username, password, re_password) VALUES(?,?,?,?)',(userid,username,password,re_password))
-            conn.commit()
+            cs.execute('INSERT INTO user (userid, username, password, re_password) VALUES(?,?,?,?)',(userid,username,password,re_password)) #The input request by the users will be inserted into the table. 
+            conn.commit() 
             conn.close()
         return redirect('/')
 
